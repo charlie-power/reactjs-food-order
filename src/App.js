96d40d17bcfc69dc279a9e3644eb03ec99cdd154
Charlie ODonnell <charles.odonnell@powerhrg.com>
2022-07-27 13:57:42 -0400

    edited appjs i guess?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler(){
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler(){
+    setCartIsShown(false);
+  }
+
   return (
     <CartProvider>
+<<<<<<< HEAD
+      <Cart />
       <Header />
+=======
+      {cartIsShown && <div>Cart...</div>}
+      <Header onShowCart={showCartHandler}/>
+>>>>>>> feat/cart-logic
       <main>
         <Meals />
       </main>
